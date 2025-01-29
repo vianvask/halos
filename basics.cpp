c4298c2a9df48897575b59e3f30465214259c89b
vianvask <91252658+vianvask@users.noreply.github.com>
2025-01-29 14:16:17 +0200

    new interpolation function

--- basics.cpp
+++ basics.cpp
@@ -17,56 +17,61 @@ double randomreal(double x1, double x2) {
     return (x1 + (x2-x1)*r01);
 }
 
+
 // linear interpolation
-double interpolate(double x, vector<vector<double> > &y) {
-    int n = y.size();
-    if (x > y[n-1][0]) {
-        //cout << "Warning: the point lies above of the interpolation range." << endl;
-        return y[n-1][1];
-    }
-    if (x < y[0][0]) {
-        //cout << "Warning: the point lies below of the interpolation range." << endl;
-        return y[0][1];
+// xy[all][0] must be strictly increasing or strictly decreasing
+double interpolate(double X, vector<vector<double> > &xy) {
+    
+    int n = xy.size();
+    auto comp0 = [&](vector<double> &XY, double val) {
+      return XY[0] < val;
+    };
+    
+    int jx;
+    if (xy[0][0] < xy[n-1][0]) {
+        jx = lower_bound(xy.begin(), xy.end(), X, comp0) - xy.begin();
+    } else {
+        jx = xy.rend() - lower_bound(xy.rbegin(), xy.rend(), X, comp0);
     }
     
-    int jmin = 0, jmax = n-1, jx;
-    while (jmax > jmin+1) {
-        jx = (int) floor((jmax+jmin)/2.0);
-        if (y[jx][0] < x) {
-            jmin = jx;
-        } else {
-            jmax = jx;
-        }
+    double Y;
+    if (jx <= 0) {
+        Y = xy[0][1];
+    } else if (jx >= n) {
+        Y = xy[n-1][1];
+    } else {
+        Y = xy[jx-1][1] + (X-xy[jx-1][0])/(xy[jx][0]-xy[jx-1][0])*(xy[jx][1]-xy[jx-1][1]);
     }
-    jx = jmin;
-    return y[jx][1] + (x - y[jx][0])/(y[jx+1][0] - y[jx][0])*(y[jx+1][1] - y[jx][1]);
+    return Y;
 }
-vector<double> interpolaten(double x, vector<vector<double> > &y) {
-    int n = y.size();
-    if (x > y[n-1][0]) {
-        //cout << "Warning: the point lies above of the interpolation range." << endl;
-        return y[n-1];
-    }
-    if (x < y[0][0]) {
-        //cout << "Warning: the point lies below of the interpolation range." << endl;
-        return y[0];
+
+// linear interpolation of vector of form {{x_1,y_11,y_21,...},{x_2,y_12,y_22,...},...}
+// xy[all][0] must be strictly increasing or strictly decreasing
+vector<double> interpolaten(double X, vector<vector<double> > &xy) {
+    
+    int n = xy.size();
+    auto comp0 = [&](vector<double> &XY, double val) {
+      return XY[0] < val;
+    };
+    
+    int jx;
+    if (xy[0][0] < xy[n-1][0]) {
+        jx = lower_bound(xy.begin(), xy.end(), X, comp0) - xy.begin();
+    } else {
+        jx = xy.rend() - lower_bound(xy.rbegin(), xy.rend(), X, comp0);
     }
     
-    int jmin = 0, jmax = n-1, jx;
-    while (jmax > jmin+1) {
-        jx = (int) floor((jmax+jmin)/2.0);
-        if (y[jx][0] < x) {
-            jmin = jx;
-        } else {
-            jmax = jx;
+    vector<double> tmp(xy[0].size(),0.0);
+    if (jx <= 0) {
+        tmp = xy[0];
+    } else if (jx >= n) {
+        tmp = xy[n-1];
+    } else {
+        tmp[0] = X;
+        for (int j = 1; j < tmp.size(); j++) {
+            tmp[j] = xy[jx-1][j] + (X-xy[jx-1][0])/(xy[jx][0]-xy[jx-1][0])*(xy[jx][j]-xy[jx-1][j]);
         }
-    }
-    jx = jmin;
-    
-    vector<double> tmp(y[0].size(),0.0);
-    tmp[0] = x;
-    for (int j = 1; j < tmp.size(); j++) {
-        tmp[j] = y[jx][j] + (x - y[jx][0])/(y[jx+1][0] - y[jx][0])*(y[jx+1][j] - y[jx][j]);
     }
     return tmp;
 }
+
--- basics.h
+++ basics.h
@@ -12,7 +12,10 @@ typedef mt19937_64 rgen;
 const double PI = 3.141592653589793238463;
 
 string to_string_prec(const double a, const int n);
+
 double randomreal(double x1, double x2, rgen &mt);
 double randomreal(double x1, double x2);
-double interpolate(double x, vector<vector<double> > &y);
-vector<double> interpolaten(double x, vector<vector<double> > &y);
+
+double interpolate(double X, vector<vector<double> > &xy);
+vector<double> interpolaten(double X, vector<vector<double> > &xy);
+
